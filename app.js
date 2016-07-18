827a4c42f64c3b6aa8edb6af08cdfffda4b670b0
J <kiendo95@gmail.com>
2016-07-18 16:49:48 +0700

    Hello 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-	res.end('Hello');
+	res.end(req.header['User-Agent']);
 })
 
 var port = process.env.PORT || 5000;
